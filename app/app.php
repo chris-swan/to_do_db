9929750be75cefd9374382cd072aec1cab736236
Kyle Pratuch & Chris Swan <student@epicodus.com>
2015-08-24 12:26:47 -0700

    add app routes

--- app/app.php
+++ app/app.php
@@ -52,6 +52,20 @@
         return $app['twig']->render('category.html.twig', array('category' =>  $category, 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
     });
 
+    $app->post("/add_tasks", function() use ($app) {
+        $category = Category::find($_POST['category_id']);
+        $task = Task::find($_POST['task_id']);
+        $category->addTask($task);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'categories' => Category::getAll(), 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
+    });
+
+    $app->post("/add_categories", function() use ($app) {
+        $category = Category::find($_POST['category_id']);
+        $task = Task::find($_POST['task_id']);
+        $task->addCategory($category);
+        return $app['twig']->render('task.html.twig', array('task' => $task, 'tasks' => Task::getAll(), 'categories' => $task->getCategories(), 'all_categories' => Category::getAll()));
+    });
+
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
         return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
